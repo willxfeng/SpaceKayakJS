1882d25af138ff072eb863b4fc54d6e07dcb40b6
Will Feng <willxfeng@gmail.com>
2016-01-15 09:12:31 -0500

    merge

--- public/javascript/src/game.js
+++ public/javascript/src/game.js
@@ -1,12 +1,12 @@
 Game = {
   WIDTH: 800,
   HEIGHT: 500,
-  BG_VOLUMN: 0.000000000000000000008,
+  BG_VOLUMN: 0.8,
   THRUSTERS_VOLUMN: 0.1,
   EXPLOSION1_VOLUMN: 0.1,
   EXPLOSION2_VOLUMN: 0.3,
 
-  newRockChance: 0.03,
+  newRockChance: 0.05,
 
   start: function() {
     Crafty.init(Game.WIDTH, Game.HEIGHT);
